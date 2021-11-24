3fae34217836846f97c6eac8f1a33f1b6bf06f58
KowerKoint <kowerkoint@gmail.com>
2021-11-24 20:35:33 +0900

    fixindent

--- library/me/template.cpp
+++ library/me/template.cpp
@@ -114,7 +114,7 @@ istream &operator>>(istream &is, pair< T1, T2 > &p) {
 template< typename T >
 ostream &operator<<(ostream &os, const vector< T > &v) {
     for(int i = 0; i < (int) v.size(); i++) {
-    os << v[i] << (i + 1 != (int) v.size() ? " " : "");
+        os << v[i] << (i + 1 != (int) v.size() ? " " : "");
     }
     return os;
 }
