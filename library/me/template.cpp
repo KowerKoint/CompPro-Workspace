4debae49c32b3e5ac94045689bcd6a444067ff9e
KowerKoint <koewrkoint@gmail.com>
2022-01-05 23:45:17 +0900

    区間更新、区間一次変換など

--- library/me/template.cpp
+++ library/me/template.cpp
@@ -173,14 +173,6 @@ template<typename S>
 value_size<S> sum_op_size(value_size<S> l, value_size<S> r) {
     return {l.value + r.value, l.size + r.size};
 };
-template<typename S>
-value_size<S> min_op_size(value_size<S> l, value_size<S> r) {
-    return {min(l.value, r.value), l.size + r.size};
-};
-template<typename S>
-value_size<S> max_op_size(value_size<S> l, value_size<S> r) {
-    return {max(l.value, r.value), l.size + r.size};
-};
 
 template<typename S>
 S min_e() { return  numeric_limits<S>::max(); };
@@ -207,6 +199,12 @@ value_size<S> add_mapping_size(F f, value_size<S> x) {
 }
 template <typename S,typename F, F ID>
 S change_mapping(F f, S x) { return (f == ID? x : f); }
+template <typename S,typename F, F ID>
+S change_mapping_size(F f, value_size<S> x) { return (f == ID? x : value_size<S>(f, x.size)); }
+template <typename S, typename F1, typename F2>
+S linear_mapping(pair<F1, F2> f, S x) { return x * f.first + f.second; }
+template <typename S, typename F1, typename F2>
+value_size<S> linear_mapping_size(pair<F1, F2> f, value_size<S> x) { return {x.value * f.first + x.size * f.second, x.size}; }
 
 template<typename F>
 F chmin_composition(F f, F g) { return min(f, g); }
@@ -214,8 +212,10 @@ template<typename F>
 F chmax_composition(F f, F g) { return max(f, g); }
 template<typename F>
 F add_composition(F f, F g) { return f + g; }
-template <typename S,typename F, F ID>
+template <typename F, F ID>
 F change_composition(F f, F g) { return (f == ID? g : f); }
+template<typename F1, typename F2>
+pair<F1, F2> linear_composition(pair<F1, F2> f, pair<F1, F2> g) { return {f.first * g.first, f.first * g.second + f.second}; }
 
 template<typename F>
 F chmin_id() { return numeric_limits<F>::max(); }
@@ -225,6 +225,8 @@ template<typename F>
 F add_id() { return 0; }
 template<typename F, F ID>
 F change_id() { return ID; }
+template<typename F1, typename F2>
+pair<F1, F2> linear_id() { return {1, 0}; }
 
 template<typename S>
 using RSumQ = segtree<S, sum_op<S>, sum_e<S>>;
@@ -251,11 +253,31 @@ using RMaxMaxQ = lazy_segtree<S, max_op<S>, max_e<S>,
 template<typename S, typename F, F ID>
 using RChangeMinQ = lazy_segtree<S, min_op<S>, min_e<S>,
     F, change_mapping<S, F, ID>,
-    change_composition<S, F, ID>,
-    change_id<F, ID>()>;
+    change_composition<F, ID>,
+    change_id<F, ID>>;
 template<typename S, typename F, F ID>
 using RChangeMaxQ = lazy_segtree<S, max_op<S>, max_e<S>,
     F, change_mapping<S, F, ID>,
-    change_composition<S, F, ID>,
-    change_id<F, ID>()>;
+    change_composition<F, ID>,
+    change_id<F, ID>>;
+template<typename S, typename F, F ID>
+using RChangeSumQ = lazy_segtree<value_size<S>, sum_op_size<S>, sum_e_size<S>,
+    F, change_mapping_size<S, F, ID>,
+    change_composition<F, ID>,
+    change_id<F, ID>>;
+template<typename S, typename F1, typename F2>
+using RLinearMinQ = lazy_segtree<S, min_op<S>, min_e<S>,
+    pair<F1, F2>, linear_mapping<S, F1, F2>,
+    linear_composition<F1, F2>,
+    linear_id<F1, F2>>;
+template<typename S, typename F1, typename F2>
+using RLinearMaxQ = lazy_segtree<S, max_op<S>, max_e<S>,
+    pair<F1, F2>, linear_mapping<S, F1, F2>,
+    linear_composition<F1, F2>,
+    linear_id<F1, F2>>;
+template<typename S, typename F1, typename F2>
+using RLinearSumQ = lazy_segtree<value_size<S>, sum_op_size<S>, sum_e_size<S>,
+    pair<F1, F2>, linear_mapping_size<S, F1, F2>,
+    linear_composition<F1, F2>,
+    linear_id<F1, F2>>;
 #endif
